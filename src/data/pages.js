080203bc8efd89e80c5aa0b7315a47e76628907f
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-11-02 00:27:43 -0500

    added multiple page functionality to pages actions

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,16 +1,26 @@
 import fetch from 'isomorphic-fetch'
 import { fetchPosts } from './posts'
 
+const REQUEST_PAGES = 'REQUEST_PAGEST'
+const RECEIVE_PAGES = 'RECEIVE_PAGES'
 const REQUEST_SINGLE_PAGE = 'REQUEST_SINGLE_PAGE'
 const RECEIVE_SINGLE_PAGE = 'RECEIVE_SINGLE_PAGE'
 
 
 const initialState = {
+  pages: [],
   currentPage: []
 }
 
 /** ACTIONS **/
-//right now, there's only actions for a single page, for a more complete theme, multiple pages (listed in a nav widget) need to be taken into consideration.
+//the request functions are here for future addition of "isFetching"
+function requestPages() {
+  return {type: REQUEST_PAGES}
+}
+
+function receivePages() {
+  return {type: RECEIVE_PAGES, pages: []}
+}
 
 function requestSinglePage(){
   return {type: REQUEST_SINGLE_PAGE}
@@ -20,7 +30,54 @@ function receiveSinglePage(json){
   return {type: RECEIVE_SINGLE_PAGE, currentPage: json}
 }
 
-function shouldFetchPages(state, pageName) {
+/** Pages Functions */
+function fetchPagesIfNeeded() {
+  return (dispatch, getState) => {
+    if (shouldFetchPages(getState())) {
+      return dispatch(fetchPages())
+    }
+  }
+}
+
+function shouldFetchPages(state) {
+  const pages = state.pageReducer.pages
+  if (pages.length === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function fetchPages() {
+  const init = {
+    method: 'GET'
+  }
+  return dispatch => {
+    dispatch(requestPages())
+    fetch('http://kevindahlberg.com/wp-json/wp/v2/pages', init)
+    .then(response => response.json())
+    .then(json => dispatch(receivePages(sanitizePages(json))))
+  }
+}
+
+function sanitizePages(pageArray) {
+  const newArray = pageArray.map((post) => {
+    let singlePage = {}
+    singlePage = {
+      title: post.title.rendered,
+      slug: post.slug,
+      content: post.content.rendered,
+      category: post.categories,
+      featuredImage: post.featured_media,
+      excerpt: post.excerpt.rendered
+    }
+    return singlePage
+  })
+  return newArray;
+}
+
+/** Single Page Functions */
+function shouldFetchSinglePages(state, pageName) {
   //this variable will get changed when multiple pages are taken into consideration
   const page = state.pageReducer.currentPage
   if (page.length !== 0 && page[0].slug === pageName) {
@@ -36,6 +93,7 @@ export function shouldFetchSinglePage(pageName) {
 
     if (shouldFetchPages(getState(), pageName)) {
       dispatch(fetchSinglePage(pageName))
+      //if the page hasn't been loaded, then the posts aren't loaded either - this improves user experience
       dispatch(fetchPosts())
     } else {
       return
@@ -59,6 +117,13 @@ function fetchSinglePage(pageName) {
 
 function pageReducer(state = initialState, action) {
   switch (action.type) {
+    case REQUEST_PAGES:
+      return state
+    case RECEIVE_PAGES:
+      return {
+        ...state,
+        pages: action.pages
+      }
     case REQUEST_SINGLE_PAGE:
       return state
     case RECEIVE_SINGLE_PAGE:
