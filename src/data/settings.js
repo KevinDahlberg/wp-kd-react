aa986b853c73782a6a143c9b335c0266fc657036
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-11-01 23:32:13 -0500

    added categories to settings

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,50 +1,75 @@
 import fetch from 'isomorphic-fetch'
 
-const REQUEST_SETTINGS = 'REQUEST_SETTINGS'
-const RECEIVE_SETTINGS = 'RECEIVE_SETTINGS'
+const REQUEST_CATEGORIES = 'REQUEST_REQUESTCATEGORIES'
+const RECEIVE_CATEGORIES = 'RECEIVE_CATEGORIES'
 
 const initialState = {
-  pageSettings: {}
+  categories: []
 }
 
 /** Actions **/
-function requestSettings() {
+function requestCategories() {
   return {
-    type: REQUEST_SETTINGS
+    type: REQUEST_CATEGORIES
   }
 }
 
-function receiveSettings(settingsObj) {
-  console.log(settingsObj);
+function receiveCategories(categoryArray) {
+  console.log(categoryArray);
   return {
-    type: RECEIVE_SETTINGS,
-    pageSettings: settingsObj
+    type: RECEIVE_CATEGORIES,
+    pageSettings: {
+      categories: categoryArray
+    }
   }
 }
 
-function shouldFetchSettings(state) {
-  const settings = state.settingsReducer.pageSettings
-  if (settings) {
+/** Category Functions */
+function fetchCategoriesIfNeeded() {
+  return (dispatch, getState) => {
+    if (shouldFetchCategories(getState())) {
+      return dispatch(fetchCategories)
+    }
+  }
+}
+
+function shouldFetchCategories(state) {
+  const categories = state.settingsReducer.pageSettings.categories
+  if (categories.length === 0) {
     return true
   } else {
     return false
   }
 }
 
-function fetchSettings () {
+function fetchCategories() {
   const init = {
     method: 'GET'
   }
   return dispatch => {
-    fetch('http://kevindahlberg.com/wp-json/wp/v2/settings/title', init)
+    dispatch(requestCategories())
+    fetch('http://kevindahlberg.com/wp-json/wp/v2/categories', init)
     .then(response => response.json())
-    .then(json => dispatch(receiveSettings(json)))
+    .then(json => dispatch(receiveCategories(json)))
   }
 }
-export function fetchSettingsIfNeeded() {
-  return (dispatch, getState) => {
-    if (shouldFetchSettings(getState())) {
-      return dispatch(fetchSettings())
-    }
+
+//Maybe add a sanitize function here.
+
+//Other Routes for site settings can be added here, such as Page Title, Page Image, and other things.
+
+function settingsReducer(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_CATEGORIES:
+      return state
+    case RECEIVE_CATEGORIES
+      return {
+        ...state,
+        categories: action.categories
+      }
+    default:
+      return state
   }
 }
+
+export default settingsReducer
